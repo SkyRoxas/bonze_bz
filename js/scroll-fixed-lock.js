606bf2a73d68f3e0abae38951d6c1c7d3026b4a3
SkyRoxas <SkyRoxas1521@gmail.com>
2016-12-12 14:07:20 +0800

    sidebar rwd

--- js/scroll-fixed-lock.js
+++ js/scroll-fixed-lock.js
@@ -17,9 +17,16 @@ jQuery(document).ready(function($) {
             'overflow-y': 'scroll',
         })
 
-        $($scrollFixedItem).parent().css({
-            'width': $($scrollFixedItem).width(),
-        })
+
+        if ($(window).width() > 1200) {
+            $($scrollFixedItem).parent().css({
+                'width': $($scrollFixedItem).width(),
+            })
+        } else(
+            $($scrollFixedItem).parent().css({
+                'width': 'auto',
+            })
+        )
 
         $startItem = '#header';
         $startItemOffset = $($startItem).offset();
@@ -70,10 +77,7 @@ jQuery(document).ready(function($) {
         })
     }
 
-
-
-
-
+    //執行fscroll_fixed_lock function
     scroll_fixed_lock('.region-sidebar-first');
 
     setTimeout(function() {
@@ -85,9 +89,6 @@ jQuery(document).ready(function($) {
         scroll_fixed_lock('.region-sidebar-first');
     });
 
-
-
-
     //防呆 disqus 後面 loading出的高
     setTimeout(function() {
         if ($('#disqus_thread').length > 0) {
@@ -95,5 +96,77 @@ jQuery(document).ready(function($) {
         }
     }, 2500);
 
+
+    //sidebarNav button
+    function sidebarNav() {
+        $parent = '#main';
+        $sidebars = '.region-sidebar-first';
+        $content = '#content';
+
+        $($content).before('<div class ="sidebarNav_button">nav</div>');
+        $('.sidebarNav_button').css({
+            'position': 'fixed',
+            'z-index': '10',
+        });
+
+        $($parent).css({
+            'position': 'relative',
+            'overflow-x': 'hidden',
+        });
+
+        $(content).css({
+            'position': 'relative',
+            'left': '0',
+        });
+
+
+
+        //toggle click function
+        var i = 0;
+        $('.sidebarNav_button').click(function() {
+
+            //sidebars animate
+
+            if (i % 2 == 0) {
+                if ($(window).width() > 1200) {
+                    $($sidebars).parent().animate({
+                        'width': '0',
+                    })
+                } else {
+                    $($content).animate({
+                        'left': $('.region-sidebar-first').innerWidth(),
+                    })
+                }
+
+
+            } else {
+                if ($(window).width() > 1200) {
+                    $($sidebars).parent().animate({
+                        'width': $('.region-sidebar-first').width(),
+                    })
+                } else {
+                    $($content).animate({
+                        'left': '0',
+                    })
+                }
+
+            }
+
+            //sidebarNav_button animate
+            if (i % 2 == 0) {
+
+
+
+            } else {
+
+
+            }
+
+
+            i++;
+        })
+    }
+
+    sidebarNav();
 
 })
