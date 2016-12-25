2ba1af98a0d988e9b090f24c4acf183cf7468e60
skyroxas <skyroxas1521@gmail.com>
2016-12-26 01:16:05 +0800

    fake sidebar

--- js/scroll_fake-scrollbar.js
+++ js/scroll_fake-scrollbar.js
@@ -4,10 +4,14 @@
 
         var $element = new Object;
         $element.item = $($scrollBarItem);
+        $element.scrollHeight = $element.item[0].scrollHeight;
         $element.width = $element.item.innerWidth();
         $element.height = $element.item.innerHeight();
-        $element.scrollTop = $element.item.scrollTop();
-        $element.scrollHeight = $element.item[0].scrollHeight;
+
+        $(window).resize(function() {
+            $element.width = $element.item.innerWidth();
+            $element.height = $element.item.innerHeight();
+        })
 
         var $fakeScrollBar = new Object;
         $fakeScrollBar.name = "fake_scrollBar";
@@ -15,40 +19,76 @@
         $fakeScrollBar.color = "#98ACC0";
         $fakeScrollBar.width = "5px";
 
-        var $browserScoll =new Object;
+        var $browserScoll = new Object;
         $browserScoll.width = 15;
+
 
         //hidden browserScoll
         $element.item.css('clip', 'rect(auto, ' + ($element.width - $browserScoll.width) + 'px, auto, auto)');
 
+        $(window).resize(function() {
+            $element.item.css('clip', 'rect(auto, ' + ($element.width - $browserScoll.width) + 'px, auto, auto)');
+        })
+
+
         //append fake_scrollBar element
         $element.item.wrapInner('<div class ="scrollBar-wrapper wrapper"></div>');
-        $element.item.children().append('<div class ="' + $fakeScrollBar.name + '"></div>');
+        $element.item.children().append('<div class ="' + $fakeScrollBar.name + '"><div class ="wrapper"><div class ="scroll-nav"></div></div></div>');
 
         $element.item.children().css('position', 'relative');
+
         $element.item.find('.' + $fakeScrollBar.name).css({
             'position': 'absolute',
             'right': '0',
-            'top': $element.scrollTop,
+            'top': $element.item.scrollTop(),
             'display': "block",
             'width': $fakeScrollBar.width,
-            'height': ($element.height / $element.scrollHeight)*100+"%",
+            'height': $(window).height(),
+            'background': 'gray',
+        })
+
+        $element.item.find('.' + $fakeScrollBar.name).children().css({
+            'position': 'relative',
+            'display': 'block',
+            'width': '100%',
+            'height': '100%',
+        })
+
+        $element.item.find('.' + $fakeScrollBar.name).children().children().css({
+            'position': 'absolute',
+            'right': '0',
+            'top': $element.item.scrollTop(),
+            'display': "block",
+            'width': '100%',
+            'height': ($element.height / $element.scrollHeight) * 100 + "%",
             'background': $fakeScrollBar.color,
         })
 
-        console.log($element.height,$element.scrollHeight);
+        $($element.item).scroll(function() {
+            $element.item.find('.' + $fakeScrollBar.name).css({
+                'top': $element.item.scrollTop(),
+            })
+            if (($element.item.scrollTop() + $(window).height()) !== $element.scrollHeight) {
+                $element.item.find('.' + $fakeScrollBar.name).children().children().css({
+                    'top': ($element.height / $element.scrollHeight) * $element.item.scrollTop(),
+                })
+            }
+
+        })
+
+
+        console.log($element.height, $element.scrollHeight);
 
     }
 
 
     $(document).ready(function() {
-      setTimeout(function(){
+        setTimeout(function() {
             scroll_fakeScrollbar('.region-sidebar-first');
-      },100)
+            //scroll_fakeScrollbar('.html');
+        }, 100)
     })
 
-    $('.region-sidebar-first').scroll(function(){
-        scroll_fakeScrollbar('.region-sidebar-first');
-    })
+
 
 })(jQuery)
